79f017d11e239195355f9eba063cc903e9ec19cd
imhyunjun <limhyunjun04@naver.com>
2020-07-23 21:00:37 +0900

    Add NewGame and TimeBoard

---
+++ Assets/Lim/Scripts/OnClickButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnClickButton : MonoBehaviour
+{
+    // 새 게임 버튼
+    public void OnClickNewGameButton()         
+    {
+        SceneManager.LoadScene("PlayGame");     //게임 씬 새로 로드
+        Time.timeScale = 1;                     //정지 풀기
+    }
+}
---
+++ Assets/Lim/Scripts/TimeScore.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TimeScore : MonoBehaviour
+{
+    Text scoreText;
+    int time;
+
+    void Start()                //초기화
+    {
+        scoreText = gameObject.GetComponent<Text>();
+        time = 0;
+        scoreText.text = "Score : " + time;
+    }
+
+   
+    void Update()
+    {
+        time = Mathf.RoundToInt(Time.timeSinceLevelLoad);       //씬이 로드 된 이후부터 시간을 정수로 반올림
+        scoreText.text = "Score : " + time;
+        
+    }
+}
