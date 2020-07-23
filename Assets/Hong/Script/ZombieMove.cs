f7f6c66307019f0f2f27cb4e0ae9367bf90b24bb
HoongGG <period_h@icloud.com>
2020-07-24 05:10:42 +0900

    좀비
    플레이어 탄생, 이동, 사망

---
+++ Assets/Hong/Script/ZombieMove.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieMove : MonoBehaviour
+{
+	public Transform target;
+	public Vector3 direction;
+	public float velocity;
+	public float accelaration;
+
+
+	// Update is called once per frame
+	void Update()
+	{
+		MoveToTarget();
+	}
+
+	public void MoveToTarget()
+	{
+		target = GameObject.Find("Player").transform;
+
+		// 플레이어의 위치와 이 객체의 위치를 빼고 단위 벡터
+		direction = (target.position - transform.position).normalized;
+
+		// 가속도
+		accelaration = 0.005f;
+
+		// 초가 아닌 한 프레임으로 가속도 계산하여 속도 증가
+		// 이거 속도가 점점 빨라지는건 좋은데 가속도에 제한을 걸고 싶어요... if문으로 하는건가요..............
+		velocity = (velocity + accelaration * Time.deltaTime);
+
+
+		// Player와 객체 간의 거리 계산
+		float distance = Vector3.Distance(target.position, transform.position);
+
+		// 30 안에있으면 따라감
+		if (distance <= 30.0f)
+		{
+			this.transform.position = new Vector3(transform.position.x + (direction.x * velocity),
+												   transform.position.y + (direction.y * velocity),
+													 transform.position.z);
+		}
+		// 30 밖으로 가면 초기화
+		else
+		{
+			velocity = 0.0f;
+		}
+	}
+}
---
+++ Assets/Hong/Script/ZombieSpawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieSpawn : MonoBehaviour
+{
+    public bool enableSpawn = false;
+    public GameObject zombie; //Prefab을 받을 public 변수 입니다.
+
+    void SpawnZombie()
+    {
+        float randomX = Random.Range(-20, 20); //적이 나타날 X좌표를 랜덤으로 생성해 줍니다.
+        float randomY = Random.Range(-20, 20); //이건 y좌표 !
+
+        if (enableSpawn)
+        {
+            GameObject Zombie = (GameObject)Instantiate(zombie, new Vector3(randomX, randomY, 0f), Quaternion.identity); //랜덤한 위치와, 화면 제일 위에서 좀비를 하나 생성함
+        }
+    }
+    void Start()
+    {
+        InvokeRepeating("SpawnZombie", 3, 1); //3초후 부터, SpawnZombie함수를 1초마다 반복해서 실행 시킵니다.
+    }
+}
