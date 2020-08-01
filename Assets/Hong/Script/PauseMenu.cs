9bde618bbd568af5a580d8d56067ea629c23c416
HoongGG <period_h@icloud.com>
2020-08-01 11:39:30 +0900

    Pause

---
+++ Assets/Hong/Script/PauseMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject PauseUI;
+    private bool paused = false;
+
+    void Start()
+    {
+        PauseUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause")) //게임세팅 인풋 19번(esc)에 퍼즈 추가함
+        {
+            paused = !paused;
+        }
+        if (paused) //일시정지
+        {
+            PauseUI.SetActive(true);
+            Time.timeScale = 0;
+        }
+
+        if (!paused)  //일시정지 해제
+        {
+            PauseUI.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void Resume() //계속하기
+    {
+        paused = !paused;
+    }
+
+    
+}
---
+++ Assets/Hong/Script/Quit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    public void QuitBtn() //종료하기
+    {
+        Debug.Log("종료");
+         Application.Quit();
+    }
+}
