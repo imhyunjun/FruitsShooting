fabf9751ae0b5a59adcb7a69b3a047b10133d7f0
HoongGG <period_h@icloud.com>
2020-08-02 13:31:33 +0900

    test

---
+++ Assets/Hong/Script/MainBtns.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainBtns : MonoBehaviour
+{
+    public void StartBtn()//버튼을 누르고 시간차를 주기위해
+    {
+        Invoke("startgame", 0.3f);
+    }
+
+    private void startgame()//플레이게임씬으로
+    {
+        SceneManager.LoadScene("PlayGame");
+    }
+
+    public void TutorialBtn()
+    {
+        Invoke("tutorial", 0.3f);
+    }
+
+    private void tutorial()//튜토리얼씬으로
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+}
