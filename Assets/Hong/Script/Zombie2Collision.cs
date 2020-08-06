2e16f190cfa52acaaff07a720ef08fa00d11f0e6
HoongGG <period_h@icloud.com>
2020-08-06 23:23:17 +0900

    ??
    error

---
+++ Assets/Hong/Script/Zombie2Collision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie2Collision : Zombie
+{
+    public GameObject zombie;
+
+    public int MaxHealth = 2;
+    public int Health = 2;
+
+
+    private void OnTriggerEnter2D(Collider2D collision) // 오렌지 태그를 가진 오브젝트를 만나면 좀비 사라짐
+    {
+        if (collision.gameObject.tag == "Orange")
+        {
+            Health -= 1;
+        }
+    }
+
+
+    public void Die()
+    {
+        if(Health == 0)
+        DestroyZombie(); }
+
+}
+
---
+++ Assets/Hong/Script/Zombie2Spawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie2Spawn : MonoBehaviour
+{
+    void SpawnZombie2()
+    {
+        float randomX = Random.Range(-20, 20); //적이 나타날 X좌표를 랜덤으로 생성해 줍니다.
+        float randomY = Random.Range(-20, 20); //이건 y좌표 !
+        var zombie = ZombieMemoryPool.GetObject();
+        var direction = new Vector2(randomX, randomY);
+        zombie.transform.position = direction;
+
+    }
+    void Start()
+    {
+        InvokeRepeating("SpawnZombie2", 3, 3); //3초후 부터, SpawnZombie함수를 1초마다 반복해서 실행 시킵니다.
+    }
+
+}
