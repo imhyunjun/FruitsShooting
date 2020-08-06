7b21937b20995b714a7d870207876a1a7c4155c3
HoongGG <period_h@icloud.com>
2020-08-06 20:36:37 +0900

    ??
    error

--- Assets/Hong/Script/ZombieSpawn.cs
+++ Assets/Hong/Script/ZombieSpawn.cs
@@ -19,6 +19,6 @@ public class ZombieSpawn : MonoBehaviour
     }
     void Start()
     {
-        InvokeRepeating("SpawnZombie", 3, 1); //3초후 부터, SpawnZombie함수를 1초마다 반복해서 실행 시킵니다.
+        InvokeRepeating("SpawnZombie", 3, 2); //3초후 부터, SpawnZombie함수를 2초마다 반복해서 실행 시킵니다.
     }
 }
